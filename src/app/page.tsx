637857ece065ef5fc19ddff3d4742d4a20c84a82
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-12-30 11:06:24 -0300

    Navbar 50%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ const perks = [
   },
   {
     name: "Para o Planeta",
-    icon: Leaf ,
+    icon: Leaf,
     description: "Comprometemos 1% da venda para a preservação e restauração do meio ambiente natural",
   }
 ]
@@ -33,7 +33,7 @@ export default function Home() {
         <p className='mt-6 text-lg max-w-prose text-muted-foreground' > Bem vindo(a) ao digital ocean. Cada ativo em nossa plataforma é verificado por nossa equipe para garantir nossos mais altos padrões de qualidade.</p>
         <div className='flex flex-col sm:flex-row gap-4 mt-6' >
           <Link href='/products' className={buttonVariants()}>Navegar pelas tendências</Link>
-          <Button variant='ghost'>Nossa promessa de qualidade </Button>
+          <Button variant='ghost'> Qualidade  &rarr;</Button>
         </div>
       </div>
     </MaxWidthWrapper>
@@ -43,7 +43,15 @@ export default function Home() {
           {perks.map((perk) => (
             <div key={perk.name} className='text-center md:flex md:items-start md:text-left lg:block lg:text-center'>
               <div className='md:flex-shrink-8 flex justify-center' >
-                <div className='h-16 w-16 flex items-center justify-center rounded-full bg-red-100 text-red-900' ></div>
+                <div className='h-16 w-16 flex items-center justify-center rounded-full bg-red-100 text-red-900' >
+                  {<perk.icon className='w-1/3 h-1/3' />}
+                </div>
+              </div>
+              <div className='mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6' >
+                <h3 className='text-base font-medium text-gray-900' >{perk.name}</h3>
+                <p className='mt-3 text-sm text-muted-foreground' >
+                  {perk.description}
+                </p>
               </div>
             </div>
           ))}
