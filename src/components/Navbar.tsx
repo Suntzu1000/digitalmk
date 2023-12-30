abf8b5894c535208cb14a2ec1a385791cb74ffeb
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-12-30 15:59:39 -0300

    Adicionando carrinho de compras

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,26 +3,46 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { Icons } from './Icons'
 import NavItems from './NavItems'
+import { buttonVariants } from './ui/button'
+import Cart from './Cart'
 
 const Navbar = () => {
+
+  const user = null
+
   return (
     <div className='bg-white sticky z-50 top-0 inset-x-0 h-16' >
-        <header className='relative bg-white' >
-            <MaxWidthWrapper>
-                <div className='border-b border-gray-200'>
-                    <div className="flex h-16 items-center" >
-                      <div className="ml-4 flex lg:ml-0">
-                        <Link href="/" >
-                          <Icons.logo className='h-10 w-10' />
-                        </Link>
-                      </div>
-                      <div className="hidden z-50 lg:ml-8 lg:block lg:self-strech">
-                        <NavItems />
-                      </div>
-                    </div>
+      <header className='relative bg-white' >
+        <MaxWidthWrapper>
+          <div className='border-b border-gray-200'>
+            <div className="flex h-16 items-center" >
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/" >
+                  <Icons.logo className='h-10 w-10' />
+                </Link>
+              </div>
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-strech">
+                <NavItems />
+              </div>
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (<Link href="/sign-in" className={buttonVariants({ variant: "ghost" })}>Entrar</Link>)}
+                  {user ? null : (<span className='h-6 w-px bg-gray-200' aria-hidden="true" />)}
+                  {user ? (<p></p>) : (<Link href="/sign-up" className={buttonVariants({ variant: "ghost" })}> Criar uma conta</Link>)}
+                  {user ? <span className='h-6 w-px bg-gray-200'></span> : null}
+                  {user ? null : (<div className='flex lg:ml-6'>
+                    <span className='h-6 w-px bg-gray-200' />
+                  </div>)}
+
+                  <div className='ml-4 flow-root lg:ml-6' >
+                    <Cart />
+                  </div>
                 </div>
-            </MaxWidthWrapper>
-        </header>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
     </div>
   )
 }
